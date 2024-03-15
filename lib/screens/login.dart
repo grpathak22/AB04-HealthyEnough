8b9d5df2845635dd587558c6f9bbcf110ddb1f8b
grpathak22 <guruprasadpathak22@gmail.com>
2024-03-16 00:49:06 +0530

    first commit made assets login page idea

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,10 +1,76 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+late Size mq;
+bool _isAnimate = false;
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Login UI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    mq = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text('Welcome to HealthyEnough',
+            style: TextStyle(
+              fontSize: 20,
+              fontFamily: 'mont',
+              fontWeight: FontWeight.w900,
+              color: Color.fromARGB(255, 0, 0, 0),
+            )),
+      ),
+      body: Stack(children: [
+        AnimatedPositioned(
+            top: mq.height * .15,
+            right: _isAnimate ? mq.width * 0.18 : mq.width * .2,
+            width: mq.width * 0.6,
+            duration: Duration(milliseconds: 600),
+            child: Image.asset('assets/images/logo-color.png')),
+        Positioned(
+            bottom: mq.height * 0.15,
+            left: mq.width * 0.05,
+            width: mq.width * 0.9,
+            height: mq.height * 0.07,
+            child: ElevatedButton.icon(
+                onPressed: () {
+                  //_handleGoogleBtnClick();
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  elevation: 8,
+                ),
+                icon: Transform.scale(
+                  scale: 0.6, // Adjust the scale factor to change the icon size
+                  child: Image.asset('assets/images/google.png'),
+                ),
+                label: RichText(
+                  text: TextSpan(children: [
+                    TextSpan(
+                        text: 'Sign in with Google',
+                        style: TextStyle(fontSize: 18, color: Colors.black)),
+                  ]),
+                )))
+      ]),
+    );
   }
 }
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,87 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:healthy_enough/screens/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _animation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(_controller);
+    _controller.forward();
+    Timer(Duration(seconds: 5), () {
+      // Navigate to the next screen when animation ends
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => LoginPage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: FadeTransition(
+          opacity: _animation,
+          child: Image.asset(
+            "assets/images/abhijeet-cid-meme.gif",
+            width: 200,
+            height: 200,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Home Screen!'),
+      ),
+    );
+  }
+}
